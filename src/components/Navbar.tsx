38be26c099097a998967ba0d251cfb06d23ad536
zuzeyka <stepan.tarasenko.26@gmail.com>
2024-12-12 18:27:47 +0100

    navigation can hidding after scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LanguageSwitcher from "@/components/LanguageSwitcher";
 import { useTranslation } from "react-i18next";
 
 const Navbar: React.FC = () => {
     const { t } = useTranslation();
+    const [showHeader, setShowHeader] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY < lastScrollY || currentScrollY <= 0) {
+            setShowHeader(true);
+        } else {
+            setShowHeader(false);
+        }
+
+        setLastScrollY(currentScrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
 
     return (
-        <nav className="bg-transparent text-white px-4 py-3 flex absolute top-0 left-0 right-0 z-50">
+        <nav
+            className={`${lastScrollY > 0 ? "bg-white/10 backdrop-blur-md" : "bg-transparent"} text-white px-4 py-3 w-full flex fixed top-0 left-0 right-0 z-50 transition-transform duration-300 ${
+                showHeader ? "translate-y-0" : "-translate-y-full"
+            }`}
+        >
             <a href="/" className="hover:opacity-50">
                 <img src="/src/assets/logo.svg" alt="Logo" className="logo" />
             </a>
             <div className="container mx-auto flex justify-center items-center">
                 <ul className="flex space-x-4 text-3xl">
+                    <li>
+                        <a href="#team" className="hover:opacity-50">
+                            {t("header.nav.team")}
+                        </a>
+                    </li>
                     <li>
                         <a href="#campaings" className="hover:opacity-50">
                             {t("header.nav.campaings")}
