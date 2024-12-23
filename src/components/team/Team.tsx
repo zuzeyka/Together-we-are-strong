356b4d0850862b7478927eeaa5d8b0ce4249259b
zuzeyka <stepan.tarasenko.26@gmail.com>
2024-12-23 19:41:57 +0100

    changed team consist

--- src/components/team/Team.tsx
+++ src/components/team/Team.tsx
@@ -11,7 +11,7 @@ const Team: React.FC = () => {
                 <h2 className="text-3xl font-bold mb-8 text-typography">
                     {t("team.title")}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                     <TeamCard
                         name={t("team.members.karina.name")}
                         position={t("team.members.karina.position")}
@@ -23,23 +23,13 @@ const Team: React.FC = () => {
                         image="/src/assets/DSC_4443.jpg"
                     />
                     <TeamCard
-                        name={t("team.members.svitlanaL.name")}
-                        position={t("team.members.svitlanaL.position")}
-                        image="/src/assets/DSC_4471.jpg"
-                    />
-                    <TeamCard
                         name={t("team.members.artur.name")}
                         position={t("team.members.artur.position")}
                         image="/src/assets/DSC_4457.jpg"
                     />
                     <TeamCard
-                        name={t("team.members.andrii.name")}
-                        position={t("team.members.andrii.position")}
-                        image="/src/assets/DSC_4463.jpg"
-                    />
-                    <TeamCard
-                        name={t("team.members.svitlanaK.name")}
-                        position={t("team.members.svitlanaK.position")}
+                        name={t("team.members.svitlana.name")}
+                        position={t("team.members.svitlana.position")}
                         image="/src/assets/DSC_4427.jpg"
                     />
                 </div>
